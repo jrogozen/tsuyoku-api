3e2bad972ea3697fa08607778151911230351129
Jon Rogozen <jrogozen@hotpads.com>
2016-01-03 14:19:52 -0800

    add travis env variables

---
+++ config/index.js
@@ -0,0 +1,14 @@
+// pretty hacky solution, conditional imports are not allowed in es6
+if (process.env.BUILD === 'local' ) {
+    const dev = require('./dev');
+
+    module.exports = dev;
+} else {
+    module.exports = {
+        jwtSecret: process.env.JWT_SECRET,
+        port: process.env.PORT,
+        db: {
+            test: process.env.TEST_DB
+        }
+    }
+}
