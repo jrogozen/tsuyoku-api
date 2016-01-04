adebcda54e48023167874610c93895878944f9b9
Jon Rogozen <jrogozen@hotpads.com>
2016-01-04 00:34:19 -0800

    make it all sudo

--- bin/applicationStart.sh
+++ bin/applicationStart.sh
@@ -11,6 +11,6 @@ $config_production_db
 pushd .
 cd /home/ec2-user/tsuyoku-api/bin
 
-NODE_ENV=production JWT_SECRET="$config_jwt_secret" PORT="$config_port" PRODUCTION_DB="$config_production_db" forever -l myApp.log -o myApp.out.log -e myApp.err.log start ../index.js
+sudo NODE_ENV=production JWT_SECRET="$config_jwt_secret" PORT="$config_port" PRODUCTION_DB="$config_production_db" forever -l myApp.log -o myApp.out.log -e myApp.err.log start ../index.js
 
 popd
--- bin/applicationStop.sh
+++ bin/applicationStop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-forever stopall 
+sudo forever stopall 
