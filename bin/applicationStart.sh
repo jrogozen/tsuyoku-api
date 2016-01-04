9acff4f4599cc54a0fd042b31a20059b1c53a556
Jon Rogozen <jrogozen@hotpads.com>
2016-01-03 22:39:07 -0800

    for the love of god

--- bin/applicationStart.sh
+++ bin/applicationStart.sh
@@ -8,4 +8,4 @@ $config_jwt_secret
 $config_port
 $config_production_db
 
-sudo NODE_ENV=production JWT_SECRET="$config_jwt_secret" PORT="$config_port" PRODUCTION_DB="$config_production_db" forever start ../index.js
+sudo NODE_ENV=production JWT_SECRET="$config_jwt_secret" PORT="$config_port" PRODUCTION_DB="$config_production_db" forever start index.js
