63e216b0f3af195327106ac996446ea301f382fe
Jon Rogozen <jrogozen@hotpads.com>
2016-01-03 23:26:23 -0800

    do not run forever in root

--- bin/applicationStart.sh
+++ bin/applicationStart.sh
@@ -9,8 +9,8 @@ $config_port
 $config_production_db
 
 pushd .
-cd /home/ec2-user/tsuyoku-api
+cd /home/ec2-user/tsuyoku-api/bin
 
-sudo NODE_ENV=production JWT_SECRET="$config_jwt_secret" PORT="$config_port" PRODUCTION_DB="$config_production_db" forever start index.js -o out.log
+NODE_ENV=production JWT_SECRET="$config_jwt_secret" PORT="$config_port" PRODUCTION_DB="$config_production_db" forever -l myApp.log -o myApp.out.log -e myApp.err.log start ../index.js
 
 popd
