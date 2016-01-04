fbdf77f949ff074f1d744a7dcf0b0705c8417992
Jon Rogozen <jrogozen@hotpads.com>
2016-01-03 22:45:48 -0800

    need to be in correct dir

--- bin/applicationStart.sh
+++ bin/applicationStart.sh
@@ -8,4 +8,9 @@ $config_jwt_secret
 $config_port
 $config_production_db
 
+pushd .
+cd /home/ec2-user/tsuyoku-api
+
 sudo NODE_ENV=production JWT_SECRET="$config_jwt_secret" PORT="$config_port" PRODUCTION_DB="$config_production_db" forever start index.js
+
+popd
