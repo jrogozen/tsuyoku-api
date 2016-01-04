00b277a362b30286907b209bd9ae2c02333c2045
Jon Rogozen <jrogozen@hotpads.com>
2016-01-04 08:05:17 -0800

    replace bcrypt with native node implementation

--- schemas/user.js
+++ schemas/user.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt-nodejs';
 
 import { errors } from '../constants';
 import { createError } from '../utils/error';
