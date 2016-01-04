4fde6180de6679491301b7e88bcc3a242e4d72ac
Jon Rogozen <jrogozen@hotpads.com>
2016-01-03 23:08:05 -0800

    fix yaml parsing

--- bin/parse_yaml.sh
+++ bin/parse_yaml.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
-parse_yaml() {
+# !/bin/sh
+
+function parse_yaml {
    local prefix=$2
    local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
-   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
         -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
    awk -F$fs '{
       indent = length($1)/2;
@@ -13,4 +15,4 @@ parse_yaml() {
          printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
       }
    }'
-}parse_yaml.sh
+}
